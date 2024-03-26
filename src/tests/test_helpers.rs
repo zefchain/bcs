63b607da02ed154bbbecb59d8c3ad8ec040c5c2f
Vivgor <mount26593@proton.me>
2024-03-26 23:43:33 +0800

    chore: upgrade dependencies

--- src/tests/test_helpers.rs
+++ src/tests/test_helpers.rs
@@ -1,6 +1,7 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+#[allow(dead_code)]
 pub fn assert_canonical_encode_decode<T>(t: T)
 where
     T: serde::Serialize + serde::de::DeserializeOwned + core::fmt::Debug + PartialEq,
